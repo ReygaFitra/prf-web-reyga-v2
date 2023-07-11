074d029f64c06a4657b0240f5c58be9e75dece1d
ReygaFitra <regafitra12@gmail.com>
2023-07-11 14:49:45 +0700

    styles: fix navigation fixed

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,32 +1,30 @@
 'use client';
-import { Flex, Heading, HStack, Button, Divider, Box, Container } from '@chakra-ui/react';
+import { Flex, Heading, HStack, Button, Divider, Box } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 const Navigation = () => {
   return (
-    <Container minW="6xl">
-      <Box pos="sticky" top={0} zIndex={20} backdropFilter="auto" backdropBlur="8px">
-        <Flex justifyContent="space-between" py="10px">
-          <Heading size="lg" color="#4E4FEB">
-            <NextLink href="/">Home</NextLink>
-          </Heading>
-          <HStack alignItems="center" gap={5} color="#4E4FEB">
-            <NextLink href="/about">About</NextLink>
-            <Divider orientation="vertical" height="25px" />
-            <NextLink href="/blog">Blog</NextLink>
-            <Divider orientation="vertical" height="25px" />
-            <NextLink href="/projects">Projects</NextLink>
-            <Divider orientation="vertical" height="25px" />
-            <NextLink href="/dashboard">Dashboard</NextLink>
-          </HStack>
-          <HStack>
-            <Button variant="outline" size="sm" alignItems="center" color="#4E4FEB">
-              Theme
-            </Button>
-          </HStack>
-        </Flex>
-      </Box>
-    </Container>
+    <Box pos="sticky" top={0} zIndex={20} backdropFilter="auto" backdropBlur="8px" boxShadow="sm">
+      <Flex justifyContent="space-between" py="10px" px="115px">
+        <Heading size="lg" color="#4E4FEB">
+          <NextLink href="/">Home</NextLink>
+        </Heading>
+        <HStack alignItems="center" gap={5} color="#4E4FEB">
+          <NextLink href="/about">About</NextLink>
+          <Divider orientation="vertical" height="25px" />
+          <NextLink href="/blog">Blog</NextLink>
+          <Divider orientation="vertical" height="25px" />
+          <NextLink href="/projects">Projects</NextLink>
+          <Divider orientation="vertical" height="25px" />
+          <NextLink href="/dashboard">Dashboard</NextLink>
+        </HStack>
+        <HStack>
+          <Button variant="outline" size="sm" alignItems="center" color="#4E4FEB">
+            Theme
+          </Button>
+        </HStack>
+      </Flex>
+    </Box>
   );
 };
 
--- src/fragments/SkillsFragment.tsx
+++ src/fragments/SkillsFragment.tsx
@@ -1,15 +1,29 @@
 'use client';
-import { TabList, Tabs, Tab } from '@chakra-ui/react';
+import { TabList, Tabs, Tab, TabPanels, TabPanel, TabIndicator } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const SkillsFragment = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabChange = (index: number) => {
+    setActiveTab(index);
+  };
+
   return (
-    <Tabs isFitted>
+    <Tabs isFitted variant="unstyled" index={activeTab} onChange={handleTabChange}>
       <TabList>
-        <Tab>Language</Tab>
-        <Tab>Frontend</Tab>
-        <Tab>Backend</Tab>
-        <Tab>Misc.</Tab>
+        <Tab _selected={{ color: '#4E4FEB' }}>Language</Tab>
+        <Tab _selected={{ color: '#4E4FEB' }}>Frontend</Tab>
+        <Tab _selected={{ color: '#4E4FEB' }}>Backend</Tab>
+        <Tab _selected={{ color: '#4E4FEB' }}>Misc.</Tab>
       </TabList>
+      <TabIndicator mt="-1.5px" height="2px" bg="#4E4FEB" borderRadius="1px" />
+      <TabPanels>
+        <TabPanel>Language tab</TabPanel>
+        <TabPanel>Frontend tab</TabPanel>
+        <TabPanel>Backend tab</TabPanel>
+        <TabPanel>Misc. tab</TabPanel>
+      </TabPanels>
     </Tabs>
   );
 };
