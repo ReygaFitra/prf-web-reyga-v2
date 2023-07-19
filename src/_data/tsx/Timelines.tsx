86c642b9173c483e5673c9640f397c4e0117a447
ReygaFitra <regafitra12@gmail.com>
2023-07-19 07:14:02 +0700

    _data: timelines data

---
+++ src/_data/tsx/Timelines.tsx
@@ -0,0 +1,22 @@
+import { Building2, GraduationCap, School } from 'lucide-react';
+
+export const Timelines = [
+  {
+    id: 1,
+    name: 'Praweda Sarana Informatika',
+    description: 'Frondend Developer Intern',
+    Icon: <Building2 />,
+  },
+  {
+    id: 2,
+    name: 'State University of Jakarta',
+    description: 'Electronics Engineering',
+    Icon: <GraduationCap />,
+  },
+  {
+    id: 1,
+    name: 'SMAN 1 Leuwiliang',
+    description: 'Mathematic and Science',
+    Icon: <School />,
+  },
+];
--- src/fragments/TimelineFragment.tsx
+++ src/fragments/TimelineFragment.tsx
@@ -2,14 +2,16 @@
 import CustomTimeline from '@/components/Timeline/CustomTimeline';
 import TitleSection from '@/components/Title/TitleSection';
 import { Box } from '@chakra-ui/react';
-import { GraduationCap } from 'lucide-react';
+import { Timelines } from '@/_data/tsx/Timelines';
 
 const TimelineFragment = () => {
   return (
     <>
       <TitleSection width="160px" headingSize="lg" headingColor="#4E4FEB" headingFont="Changa" name="Education" dividerColor="#4E4FEB" />
       <Box my="20px">
-        <CustomTimeline timeline="Example" description="Example" timelineIcon={<GraduationCap />} />
+        {Timelines.map((timeline) => (
+          <CustomTimeline timeline={timeline.name} description={timeline.description} timelineIcon={timeline.Icon} />
+        ))}
       </Box>
     </>
   );
