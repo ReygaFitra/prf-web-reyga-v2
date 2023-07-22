44590da36d8dbc699549101d1da7ec1305e65193
ReygaFitra <regafitra12@gmail.com>
2023-07-22 20:00:13 +0700

    assets: add projects image

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,15 +1,23 @@
-import { Box, Button, Container, FormControl, FormLabel, Input, Text, Textarea } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, FormControl, FormLabel, Input, Text, Textarea } from '@chakra-ui/react';
 import { HorizontalDivider } from '../Divider/CustomDivider';
 import Frame from '../Frame/Frame';
+import { Mail } from 'lucide-react';
 
 const ContactForm = () => {
   return (
     <Container minW="xl" my="20px">
       <FormControl w="100%" color="#ECF8F9">
         <Frame w="100%" h="100%" bgColor="#0E2954" rounded="none" p="20px" boxShadow="15px 15px #4E4FEB" overflow="hidden">
-          <Text fontStyle={'italic'} color="#ECF8F9" my="10px">
+          <Text fontStyle="italic" color="#ECF8F9" my="10px">
             Or you can send me a message via email :
           </Text>
+          <Flex gap={2} alignItems="center">
+            <Mail color="#ECF8F9" />
+            <Text fontStyle="semibold" color="#ECF8F9" my="5px">
+              Regafitra12@gmail.com
+            </Text>
+          </Flex>
+
           <HorizontalDivider width="100%" height="3px" color="#4E4FEB" />
           <Box my="25px">
             <FormLabel>Name :</FormLabel>
