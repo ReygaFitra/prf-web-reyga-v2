f8d398b8c46aff5c52761482b08f045f5de96cb0
ReygaFitra <regafitra12@gmail.com>
2023-07-22 20:08:13 +0700

    _data: add projects tsx data

---
+++ src/_data/tsx/Projects.tsx
@@ -0,0 +1,39 @@
+import Projects1 from '@/assets/img/projects/project1.jpg';
+import Projects2 from '@/assets/img/projects/project2.jpg';
+import Projects3 from '@/assets/img/projects/project3.jpg';
+import Projects4 from '@/assets/img/projects/project4.jpg';
+import Projects5 from '@/assets/img/projects/project5.jpg';
+import Projects6 from '@/assets/img/projects/project6.jpg';
+
+export const Projects = [
+  {
+    id: 1,
+    img: Projects1,
+    href: '/',
+  },
+  {
+    id: 2,
+    img: Projects2,
+    href: '/',
+  },
+  {
+    id: 3,
+    img: Projects3,
+    href: '/',
+  },
+  {
+    id: 4,
+    img: Projects4,
+    href: '/',
+  },
+  {
+    id: 5,
+    img: Projects5,
+    href: '/',
+  },
+  {
+    id: 6,
+    img: Projects6,
+    href: '/',
+  },
+];
