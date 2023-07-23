1a595be5e9d14ce0b96b5109c14ba77c7b38ec3e
ReygaFitra <regafitra12@gmail.com>
2023-07-23 10:29:49 +0700

    styles: add  images priority property

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = (props: Hero) => {
         </Text>
         <ButtonLink href="/about" color="#4E4FEB" variant="outline" size="md" title="About Me" radius="sm" />
         <Frame maxW="300px" maxH="300px" rounded="sm" my="20px" bgColor="#0E2954" p="10px" boxShadow={'13px 13px #4E4FEB'} overflow="hidden">
-          <Image src={hamster} alt="source: https://unsplash.com/photos/D_pXn7cueOs" style={{ objectFit: 'cover', alignItems: 'center', height: '100%', width: '100%' }} />
+          <Image priority={true} src={hamster} alt="source: https://unsplash.com/photos/D_pXn7cueOs" style={{ objectFit: 'cover', alignItems: 'center', height: '100%', width: '100%' }} />
         </Frame>
       </VStack>
     </Flex>
--- src/fragments/ProjectsFragment.tsx
+++ src/fragments/ProjectsFragment.tsx
@@ -1,37 +1,45 @@
 import Frame from '@/components/Frame/Frame';
 import TitleSection from '@/components/Title/TitleSection';
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Box, Button, Flex, Grid, GridItem, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import { Projects } from '@/_data/tsx/Projects';
+import { HorizontalDivider } from '@/components/Divider/CustomDivider';
+import { File, Link, MonitorPlay } from 'lucide-react';
 
 const ProjectsFragment = () => {
   return (
     <>
       <TitleSection width="200px" headingSize="lg" headingColor="#4E4FEB" headingFont="Changa" name="Projects" dividerColor="#4E4FEB" />
       <Grid gridTemplateColumns="repeat(3, 1fr)" my="20px" gap={7}>
-        <GridItem>
-          <Frame bgColor="#0E2954" maxW="100%" minH="330px" rounded="none" boxShadow="13px 13px #4E4FEB" overflow="hidden">
-            Projects
-          </Frame>
-        </GridItem>
-        <GridItem>
-          <Frame bgColor="#0E2954" maxW="100%" minH="330px" rounded="none" boxShadow="13px 13px #4E4FEB" overflow="hidden">
-            Projects
-          </Frame>
-        </GridItem>
-        <GridItem>
-          <Frame bgColor="#0E2954" maxW="100%" minH="330px" rounded="none" boxShadow="13px 13px #4E4FEB" overflow="hidden">
-            Projects
-          </Frame>
-        </GridItem>
-        <GridItem>
-          <Frame bgColor="#0E2954" maxW="100%" minH="330px" rounded="none" boxShadow="13px 13px #4E4FEB" overflow="hidden">
-            Projects
-          </Frame>
-        </GridItem>
-        <GridItem>
-          <Frame bgColor="#0E2954" maxW="100%" minH="330px" rounded="none" boxShadow="13px 13px #4E4FEB" overflow="hidden">
-            Projects
-          </Frame>
-        </GridItem>
+        {Projects.map((project, index) => (
+          <GridItem key={index}>
+            <Frame bgColor="#0E2954" maxW="100%" minH="330px" boxShadow="13px 13px #4E4FEB" overflow="hidden" p="5px" rounded="sm">
+              <Image priority={true} src={project.img} alt={`Project Image ${project.id}`} style={{ objectFit: 'cover', minHeight: '250px', width: '100%' }} />
+              <Box display="flex" gap={2} my="10px" justifyContent="end">
+                <Button size="sm" variant="ghost" rounded="sm" _hover={{ bgColor: 'rgba(255, 255, 255, 0.3)' }}>
+                  <Flex alignItems="center" gap={1}>
+                    <Text color={'#ECF8F9'}>Detail</Text>
+                    <File color="#ECF8F9" size="20px" />
+                  </Flex>
+                </Button>
+                <Button size="sm" variant="ghost" rounded="sm" _hover={{ bgColor: 'rgba(255, 255, 255, 0.3)' }}>
+                  <Flex alignItems="center" gap={1}>
+                    <Text color={'#ECF8F9'}>Demo</Text>
+                    <MonitorPlay color="#ECF8F9" size="20px" />
+                  </Flex>
+                </Button>
+                <Button size="sm" variant="ghost" rounded="sm" _hover={{ bgColor: 'rgba(255, 255, 255, 0.3)' }}>
+                  <Flex alignItems="center" gap={1}>
+                    <Text color={'#ECF8F9'}>Github</Text>
+                    <Link color="#ECF8F9" size="20px" />
+                  </Flex>
+                </Button>
+              </Box>
+
+              <HorizontalDivider width="100%" height="3px" color="#4E4FEB" />
+            </Frame>
+          </GridItem>
+        ))}
       </Grid>
     </>
   );
